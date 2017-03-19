455eb9ce2efb04ef9ad289770c63c2761709ee5f
metherton <martinetherton63@gmail.com>
2017-03-19 21:16:57 +0100

    add test for routing component

--- client/src/app/app-routing/app-routing.component.spec.ts
+++ client/src/app/app-routing/app-routing.component.spec.ts
@@ -1,43 +1,29 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {Http, Response} from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
-import 'rxjs/add/operator/toPromise';
-import { CityService } from '../services/city.service';
-import { AppRoutingComponent } from './app-routing.component';
 
-describe('AppRoutingComponent', () => {
-  let cityService: CityService;
-  let http: Http;
+import { AppRoutingComponent } from './app-routing.component';
+import {CityService} from "../services/city.service";
+
+describe('App Routing Component', () => {
   let component: AppRoutingComponent;
   let fixture: ComponentFixture<AppRoutingComponent>;
 
   beforeEach(async(() => {
-
-    let cityServiceStub = {
-      getCities: jasmine.createSpy('getCities')
-    };
-
     TestBed.configureTestingModule({
-      imports: [HttpModule],
       declarations: [ AppRoutingComponent ],
-      providers: [{provide: CityService, useValue: cityServiceStub}, {provide: XHRBackend, useValue: MockBackend}]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [{provide: CityService, useValue: {getCities: jasmine.createSpy('getCities')}}]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AppRoutingComponent);
     component = fixture.componentInstance;
-    // CityService actually injected into the component
-    cityService = fixture.debugElement.injector.get(CityService);
-    http = fixture.debugElement.injector.get(Http);
-    // Setup spy on the `getCities` method
-    spy = spyOn(cityService, 'getCities')
-      .and.returnValue(Promise.resolve(['London', 'New York']));
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
